2b0b3a090fbf4e733b54830afa4193e1c1522e34
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-05-17 18:04:55 -0300

    aula 2 - adicionado pequena borda nas linhas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full"/>
         
         {/* linhas */}
-        <div className="absolute right-0 top-0 bottom-0 w-2 bg-stripes"/>
+        <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes"/>
 
       </div>
 
