1cb0ba06ae37dfd14cfdf8a83b901d609f7733ad
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-05-17 18:03:47 -0300

    aula 2 - adicionado linhas

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,11 @@ export default function Home() {
     <main className="grid grid-cols-2 min-h-screen ">
       {/* esquerda */}
       <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden">
+        {/* blur */}
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full"/>
+        
+        {/* linhas */}
+        <div className="absolute right-0 top-0 bottom-0 w-2 bg-stripes"/>
 
       </div>
 
