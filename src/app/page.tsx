74d2d12cbf199bbb4c88fc6473b8c72f9d2ab1e1
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-05-17 22:16:57 -0300

    aula 2 - criado hero com logo e detalhes do site

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,7 @@
 import { User } from 'lucide-react';
+import Image from 'next/image';
+ 
+import nlwLogo from '../assets/nlw-spacetime-logo.svg';
 
 export default function Home() {
   return (
@@ -20,7 +23,25 @@ export default function Home() {
           <p className='text-sm leading-snug max-w-[140px]'> 
             <span className='underline'>Crie sua conta</span> e salve suas memórias!
           </p>
-        </a>        
+        </a>  
+
+        {/* Hero */}
+        <div className='space-y-5'>
+          <Image src={nlwLogo} alt='NLW Spacetime logo'/>
+
+          <div className='max-w-[420px] space-y-1'>
+            <h1 className='text-5xl font-bold leading-tight text-gray-50'>
+              Sua cápsula do tempo
+            </h1>
+            <p className='text-lg leading-relaxed'>
+              Colecione momentos marcantes da sua jornada e compartilhe (se quiser) com o mundo!
+            </p>
+          </div>
+
+          <a href="" className='inline-block rounded-full bg-green-500 px-5 py-3 font-alt text-sm uppercase leading-none text-black hover:bg-green-600'>
+            CADASTRAR LEMBRANÇA
+          </a>
+        </div>
 
       </div>
 
