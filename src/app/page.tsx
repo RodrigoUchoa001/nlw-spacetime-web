031055469fc93e22a35f45db0609651bbc5cf3d7
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-05-17 22:23:22 -0300

    aula 2 - adicionado msg de copyright

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,6 +43,19 @@ export default function Home() {
           </a>
         </div>
 
+        {/* Copyright */}
+        <div className='text-sm leading-relaxed text-gray-200'>
+          Feito com 💜 no NLW da {' '}
+          <a 
+            target='_blank' //PRA ABRIR UMA NOVA ABA
+            rel='noreferrer' //PRA EVITAR UNS PROBLEMA DE SEGURANÇA
+            href="https://rocketseat.com.br" 
+            className='underline hover:text-gray-100'
+          >
+              Rocketseat
+          </a>
+        </div>
+
       </div>
 
       {/* direita */}
