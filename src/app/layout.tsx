7c2a5be339f8c8314496d73c395c5e6d13bfcb20
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-05-21 17:21:38 -0300

    aula 4 - movido conteudo que se repete em todas as paginas para o arquivo layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,11 @@
+import EmptyMemories from '@/components/EmptyMemories'
+import Hero from '@/components/Hero'
+import SignIn from '@/components/SignIn'
+import Profile from '@/components/profile'
 import './globals.css'
 import { Roboto_Flex, Bai_Jamjuree } from 'next/font/google'
+import Copyright from '@/components/Copyright'
+import { cookies } from 'next/headers'
 
 const roboto = Roboto_Flex({ subsets: ['latin'], variable: '--font-roboto' })
 const baiJamjuree = Bai_Jamjuree({ subsets: ['latin'], weight: '700', variable: '--font-jam-juree' })
@@ -14,10 +20,32 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  
+  const isAuthenticated = cookies().has('token');
+
   return (
     <html lang="en">
       <body className={`${roboto.variable} ${baiJamjuree.variable} font-sans bg-gray-900 text-gray-100`}>
+        <main className="grid grid-cols-2 min-h-screen ">
+      {/* esquerda */}
+      <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover">
+        {/* blur */}
+        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full"/>
+        
+        {/* linhas */}
+        <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes"/>
+        
+        {isAuthenticated? <Profile /> : <SignIn /> }
+        <Hero />
+        <Copyright />
+
+      </div>
+
+      {/* direita */}
+      <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
         {children}
+      </div>
+    </main>
       </body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,8 @@
-import { cookies } from 'next/headers';
-
-import Copyright from '@/components/Copyright';
-import Hero from '@/components/Hero';
-import SignIn from '@/components/SignIn';
 import EmptyMemories from '@/components/EmptyMemories';
-import Profile from '@/components/profile';
 
 export default function Home() {
-  const isAuthenticated = cookies().has('token');
 
   return (
-    <main className="grid grid-cols-2 min-h-screen ">
-      {/* esquerda */}
-      <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover">
-        {/* blur */}
-        <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full"/>
-        
-        {/* linhas */}
-        <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes"/>
-        
-        {isAuthenticated? <Profile /> : <SignIn /> }
-        <Hero />
-        <Copyright />
-
-      </div>
-
-      {/* direita */}
-      <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
-        <EmptyMemories />
-      </div>
-    </main>
+    <EmptyMemories />
   )
 }
