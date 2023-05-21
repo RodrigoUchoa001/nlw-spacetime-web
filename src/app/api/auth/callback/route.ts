47820a9c9157c6a18e877c077ec48ee5e3629dbd
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2023-05-20 23:36:09 -0300

    criada rota para comunicar com API e salvar cookie com token de usuário

---
+++ src/app/api/auth/callback/route.ts
@@ -0,0 +1,28 @@
+import { api } from "@/lib/api";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+    const { searchParams } = new URL(req.url);
+    const code = searchParams.get('code') ;
+
+    const registerResponse = await api.post('/register', {
+        code,
+    });
+
+    const { token } = registerResponse.data;
+
+    const redirectURL = new URL('/', req.url);
+
+    const cookieExpiresInSeconds = 60 * 60 * 24 * 30; //um mês
+
+    return NextResponse.redirect(redirectURL, {
+        headers: {
+            'Set-Cookie': `token=${token}; Path=/; max-age=${cookieExpiresInSeconds}` //adicionado o token aos cookies
+            // token: o nome do cookie
+            // Path: o rota em q o cookie vai estar disponivel, se fosse "/auth", 
+            // só nas rotas depois de /auth o cookie existiria, do jeito q está, tá disponivel
+            // em toda a aplicação
+            // max-age: tempo q o cookie estará disponivel
+        },
+    });
+}
